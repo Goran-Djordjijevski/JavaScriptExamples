7d240501dd60da5a5d2e37ca477c90926da03e21
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-06-26 15:01:55 +0200

    Added new string.html and string.js files.

---
+++ javascript/string.js
@@ -0,0 +1,13 @@
+/* Strings */
+
+var firstName = "Goran";
+var secondName = "Djordjijevski";
+
+var fullName = firstName + " " + secondName;
+console.log(fullName);
+
+var city = " Zrenjanin";
+
+fullName += city;
+
+console.log(fullName);
