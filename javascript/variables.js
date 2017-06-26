4a7f31c987741a8a00496a8220c3b2f184aa2b06
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-06-26 10:29:07 +0200

    Added new files variables.html and variables.js files.

---
+++ javascript/variables.js
@@ -0,0 +1,11 @@
+/* Variables */
+
+var myName = "Goran";
+
+var city = "Zrenjanin";
+
+console.log("My name is " + myName + " and I live in " + city);
+
+city = "Novi Sad";
+
+console.log("My name is " + myName + " and I live in " + city);
