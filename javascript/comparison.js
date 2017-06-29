0b5c41a58622e884c57dc3b69249f0921987bfe2
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-06-29 12:31:55 +0200

    Added new comparison.html and comparison.js files.

---
+++ javascript/comparison.js
@@ -0,0 +1,23 @@
+/* Comparison operators and If and If-Else statements */
+
+var isGood = true;
+
+if (isGood === true) {
+    console.log("This is true and it's work");
+} else {
+    console.log("This is false and still works");
+}
+
+// Comparison Operators: > < <= >= == !=
+
+var age = 18;
+
+if (age >= 18) {
+    console.log("You are an adult");
+} else if (age < 18) {
+    console.log("You are a child");
+}
+
+if (age === 18) {
+    console.log("You are 18 years old. Congratulation");
+}
