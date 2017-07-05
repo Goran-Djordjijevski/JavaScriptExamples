0692583a3eabf9088558807f16e62f3ad9691a4b
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-07-05 21:00:55 +0200

    Added new whileloop.html and whileloop.js files.

---
+++ javascript/whileloops.js
@@ -0,0 +1,15 @@
+/* While loops and do while loops */
+
+var n = 0;
+
+while (n < 5) {
+    console.log("n is " + n);
+    n++;
+}
+
+var i = 0;
+
+do {
+    console.log("i is " + i);
+    i++;
+} while (i < 10);
