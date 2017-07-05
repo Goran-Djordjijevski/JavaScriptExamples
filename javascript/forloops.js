64762d675fa7952a12417c48baaa1891757afb54
Goran Djordjijevski <goran-djordjijevski@outlook.com>
2017-07-05 20:51:17 +0200

    Added new forloops.html and forloops.js files.

---
+++ javascript/forloops.js
@@ -0,0 +1,14 @@
+/* For loops */
+
+var demoArray = [];
+for (var i = 0; i < 10; i++) {
+    demoArray.push(i);
+}
+
+console.log(demoArray);
+
+var numbersArray = [1, 2, 3, 4, 5];
+for (var i = 0; i < numbersArray.length; i++) {
+    console.log(numbersArray[i]);
+}
+
